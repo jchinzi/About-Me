c557e458fe311c92fe59717b002883671dfecb18
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-02 15:35:37 -0700

    Fix link to js and css files

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 'use strict';
 
+// Variables==================================
+
+var schoolGuess;
+var petGuess;
+var majorGuess;
+var fakeJob;
+var realJob;
+
+var guestName;
+
+// Guessing Game==============================
+
+guestName = prompt('Welcome to my About Me page!  What should I call you?');
+
