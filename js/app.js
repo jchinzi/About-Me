26b399ff00ca2768bb93ff1b9892f8c51fd17b4e
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-02 17:49:45 -0700

    Comment out console.logs of quiz answers and replace with alerts per assignment instructions

--- js/app.js
+++ js/app.js
@@ -30,10 +30,14 @@ if (wantToPlay){
 var lowPetGuess = petGuess.toLowerCase();
 
 if (lowPetGuess === 'no' || lowPetGuess === 'n'){
-  console.log('That\'s right!  My mom has cats, but I have sugar gliders!');
+  //console.log('That\'s right!  My mom has cats, but I have sugar gliders!');
+  alert('That\'s right!  My mom has cats, but I have sugar gliders!');
 } else if (lowPetGuess === 'yes' || lowPetGuess === 'y'){
-  console.log('Nope, you must have me confused with someone else.');
-} else {console.log('Yes or No answers only, please.');}
+  //console.log('Nope, you must have me confused with someone else.');
+  alert('Nope, you must have me confused with someone else.');
+} else {
+  //console.log('Yes or No answers only, please.');}
+  alert('Yes or No answers only, please.');}
 
 
 schoolGuess = prompt('Did I go to the University of Washington?');
@@ -41,10 +45,14 @@ schoolGuess = prompt('Did I go to the University of Washington?');
 var lowSchoolGuess = schoolGuess.toLowerCase();
 
 if (lowSchoolGuess === 'no' || lowSchoolGuess === 'n'){
-  console.log('That\'s right!  I\'m a proud Banana Slug!');
+  //console.log('That\'s right!  I graduated from UCSC and am a proud Banana Slug!');
+  alert('That\'s right!  I graduated from UCSC and am a proud Banana Slug!');
 } else if (lowSchoolGuess === 'yes' || lowSchoolGuess === 'y'){
-  console.log('Nope, I\'ve actually never taken a class at UW');
-} else {console.log('Yes or No answers only, please.');}
+  //console.log('Nope, I\'ve actually never taken a class at UW');
+  alert('Nope, I\'ve actually never taken a class at UW');
+} else {
+  //console.log('Yes or No answers only, please.');}
+  alert('Yes or No answers only, please.');}
 
 
 majorGuess = prompt('Did I study Literature in college?');
@@ -52,10 +60,14 @@ majorGuess = prompt('Did I study Literature in college?');
 var lowmajorGuess = majorGuess.toLowerCase();
 
 if (lowmajorGuess === 'no' || lowmajorGuess === 'n'){
-  console.log('Sorry, that is incorrect.  In fact, Literature was my major!');
+  //console.log('Sorry, that is incorrect.  In fact, Literature was my major!');
+  alert('Sorry, that is incorrect.  In fact, Literature was my major!');
 } else if (lowmajorGuess === 'yes' || lowmajorGuess === 'y'){
-  console.log('You\'re right!  I majored in Literature.');
-} else {console.log('Yes or No answers only, please.');}
+  //console.log('You\'re right!  I majored in Literature.');
+  alert('You\'re right!  I majored in Literature.');
+} else {
+  //console.log('Yes or No answers only, please.');}
+  alert('Yes or No answers only, please.');}
 
 
 fakeJob = prompt('Have I worked as a waitress?');
@@ -63,10 +75,14 @@ fakeJob = prompt('Have I worked as a waitress?');
 var lowfakeJob = fakeJob.toLowerCase();
 
 if (lowfakeJob === 'no' || lowfakeJob === 'n'){
-  console.log('Correct!  I\'ve had quite a few jobs, but I\'ve never worked as a waitress');
+  //console.log('Correct!  I\'ve had quite a few jobs, but I\'ve never worked as a waitress');
+  alert('Correct!  I\'ve had quite a few jobs, but I\'ve never worked as a waitress');
 } else if (lowfakeJob === 'yes' || lowfakeJob === 'y'){
-  console.log('Nope!  Surprising, I know, but I\'ve never worked as a waitress.');
-} else {console.log('Yes or No answers only, please.');}
+  //console.log('Nope!  Surprising, I know, but I\'ve never worked as a waitress.');
+  alert('Nope!  Surprising, I know, but I\'ve never worked as a waitress.');
+} else {
+  //console.log('Yes or No answers only, please.');}
+  alert('Yes or No answers only, please.');}
 
 
 realJob = prompt('Have I ever worked in the performing arts?');
@@ -74,11 +90,19 @@ realJob = prompt('Have I ever worked in the performing arts?');
 var lowrealJob = realJob.toLowerCase();
 
 if (lowrealJob === 'no' || lowrealJob === 'n'){
-  console.log('Sorry, that is incorrect.  I actually worked for a theater company for several years while I was a college student!');
+  //console.log('Sorry, that is incorrect.  I actually worked for a theater company for several years while I was a college student!');
+  alert('Sorry, that is incorrect.  I actually worked for a theater company for several years while I was a college student!');
 } else if (lowrealJob === 'yes' || lowrealJob === 'y'){
-  console.log('You\'re right!  I worked as an electrician for a theater company when I was a college student.');
-} else {console.log('Yes or No answers only, please.');}
+  //console.log('You\'re right!  I worked as an electrician for a theater company when I was a college student.');
+  alert('You\'re right!  I worked as an electrician for a theater company when I was a college student.');
+} else {
+  //console.log('Yes or No answers only, please.');}
+  alert('Yes or No answers only, please.');}
+
 
 if ((lowPetGuess === 'no' || lowPetGuess === 'n') && (lowSchoolGuess === 'no' || lowSchoolGuess === 'n') && (lowmajorGuess === 'yes' || lowmajorGuess === 'y') && (lowfakeJob === 'no' || lowfakeJob === 'n') && (lowrealJob === 'yes' || lowrealJob === 'y')){
-  console.log('Congratulations ' + guestName + '! You got every question right!  I look forward to getting to know you as well.');
-} else {console.log('Thanks for playing my game, ' + guestName + '. I look forward to getting to know you as well.');}
+  //console.log('Congratulations ' + guestName + '! You got every question right!  I look forward to getting to know you as well.');
+  alert('Congratulations ' + guestName + '! You got every question right!  I look forward to getting to know you as well.');
+} else {
+  //console.log('Thanks for playing my game, ' + guestName + '. I look forward to getting to know you as well.');}
+  alert('Thanks for playing my game, ' + guestName + '. I look forward to getting to know you as well.');}
