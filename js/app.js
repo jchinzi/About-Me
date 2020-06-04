6fb95966e2c6b98854d4ca0e152c58437e87c5ac
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-03 18:05:16 -0700

    Add a score element and final message to js

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ var countryVisited;
 var guestName;
 var wantToPlay;
 
+var score = 0;
+
 // ================Greeting ======================
 
 guestName = prompt('Welcome to my About Me page!  What should I call you?');
@@ -37,12 +39,14 @@ var lowPetGuess = petGuess.toLowerCase();
 if (lowPetGuess === 'no' || lowPetGuess === 'n'){
   //console.log('That\'s right!  My mom has cats, but I have sugar gliders!');
   alert('That\'s right!  My mom has cats, but I have sugar gliders!');
+  score++;
 } else if (lowPetGuess === 'yes' || lowPetGuess === 'y'){
   //console.log('Nope, you must have me confused with someone else.');
   alert('Nope, you must have me confused with someone else.');
 } else {
   //console.log('Yes or No answers only, please.');}
   alert('Yes or No answers only, please.');}
+
 
 // =================Q2 - False School=========================
 
@@ -53,6 +57,7 @@ var lowSchoolGuess = schoolGuess.toLowerCase();
 if (lowSchoolGuess === 'no' || lowSchoolGuess === 'n'){
   //console.log('That\'s right!  I graduated from UCSC and am a proud Banana Slug!');
   alert('That\'s right!  I graduated from UCSC and am a proud Banana Slug!');
+  score++;
 } else if (lowSchoolGuess === 'yes' || lowSchoolGuess === 'y'){
   //console.log('Nope, I\'ve actually never taken a class at UW');
   alert('Nope, I\'ve actually never taken a class at UW');
@@ -72,6 +77,7 @@ if (lowmajorGuess === 'no' || lowmajorGuess === 'n'){
 } else if (lowmajorGuess === 'yes' || lowmajorGuess === 'y'){
   //console.log('You\'re right!  I majored in Literature.');
   alert('You\'re right!  I majored in Literature.');
+  score++;
 } else {
   //console.log('Yes or No answers only, please.');}
   alert('Yes or No answers only, please.');}
@@ -85,6 +91,7 @@ var lowfakeJob = fakeJob.toLowerCase();
 if (lowfakeJob === 'no' || lowfakeJob === 'n'){
   //console.log('Correct!  I\'ve had quite a few jobs, but I\'ve never worked as a waitress');
   alert('Correct!  I\'ve had quite a few jobs, but I\'ve never worked as a waitress');
+  score++;
 } else if (lowfakeJob === 'yes' || lowfakeJob === 'y'){
   //console.log('Nope!  Surprising, I know, but I\'ve never worked as a waitress.');
   alert('Nope!  Surprising, I know, but I\'ve never worked as a waitress.');
@@ -104,6 +111,7 @@ if (lowrealJob === 'no' || lowrealJob === 'n'){
 } else if (lowrealJob === 'yes' || lowrealJob === 'y'){
   //console.log('You\'re right!  I worked as an electrician for a theater company when I was a college student.');
   alert('You\'re right!  I worked as an electrician for a theater company when I was a college student.');
+  score++;
 } else {
   //console.log('Yes or No answers only, please.');}
   alert('Yes or No answers only, please.');}
@@ -117,6 +125,7 @@ for (var i = 0; i <4; i++){ //Allows up to 4 guesses
 
   if (numStatesVisited===22){
     alert('That\'s exactly right! How did you know?');
+    score++;
     break; //stops loop early in the case of a correct answer
   } else if (numStatesVisited>22 && numStatesVisited<50){
     alert('Not quite that many');
@@ -135,6 +144,7 @@ for (i = 0; i<6; i++){ //Allows up to 6 guesses
 
   if (countryVisited === countryArray[0]||countryVisited === countryArray[1]||countryVisited === countryArray[2]||countryVisited === countryArray[3]||countryVisited === countryArray[4]||countryVisited === countryArray[5]){
     alert('That\'s right!  How did you know?');
+    score++;
     break;
 
   } else if (countryVisited !== countryArray[0] && countryVisited !== countryArray[1] && countryVisited !== countryArray[2] && countryVisited !== countryArray[3] && countryVisited !== countryArray[4] && countryVisited !== countryArray[5]){
@@ -144,11 +154,9 @@ for (i = 0; i<6; i++){ //Allows up to 6 guesses
 }
 alert('I\'d like to travel more extensively, but so far I\'ve visited Ecuador, Ireland, England, Wales, Scotland & Canada.');
 
-// =================Final Message=========================
+// =================Final Score=========================
 
-if ((lowPetGuess === 'no' || lowPetGuess === 'n') && (lowSchoolGuess === 'no' || lowSchoolGuess === 'n') && (lowmajorGuess === 'yes' || lowmajorGuess === 'y') && (lowfakeJob === 'no' || lowfakeJob === 'n') && (lowrealJob === 'yes' || lowrealJob === 'y')){
-  //console.log('Congratulations ' + guestName + '! You got every question right!  I look forward to getting to know you as well.');
-  alert('Congratulations ' + guestName + '! You got every question right!  I look forward to getting to know you as well.');
+if (score > 6){
+  alert('Congratulations ' + guestName + '! You got all ' + score +' questions right!  I hope you enjoyed the game!');
 } else {
-  //console.log('Thanks for playing my game, ' + guestName + '. I look forward to getting to know you as well.');}
-  alert('Thanks for playing my game, ' + guestName + '. I look forward to getting to know you as well.');}
+  alert('Congratulations ' + guestName + '. You got a score of ' + score + ' out of 7.  If you\'d like to play again, just refresh the page!');}
